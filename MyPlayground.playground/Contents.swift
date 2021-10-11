29ec59f2457fb97aa52d4cb17d970d66cfe5fb66
Ehab hakami <ehab7k@gmail.com>
2021-10-11 18:51:20 +0300

    ehab home work1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import Foundation
+var firstName:String = "ehab"
+
+var sacndName:String = " Ahmed "
+var myAge:String = "24"
+firstName = "my name is " + firstName + sacndName + "my age is " + myAge
+print(firstName)
+
+
